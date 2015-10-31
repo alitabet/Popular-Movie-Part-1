e0c59954866cfc04ce05fb56cc03d016a44c9e2b
alitabet <alitabet@gmail.com>
2015-10-31 16:14:42 +0300

    Add initial MovieDB Contract and MovieDB Helper

---
+++ app/src/main/java/com/example/android/movies/data/MovieContract.java
@@ -0,0 +1,30 @@
+package com.example.android.movies.data;
+
+import android.provider.BaseColumns;
+
+/**
+ * Define table and column names for Popular Movies DB
+ */
+public class MovieContract {
+
+    /*
+        Inner class that defines the contents of the movie table
+     */
+    public static final class MovieEntry implements BaseColumns {
+
+        public static final String TABLE_NAME = "movie";
+
+        // Movie title
+        public static final String COLUMN_TITLE = "title";
+        // Movie synopsis
+        public static final String COLUMN_SYNOPSIS = "synopsis";
+        // Release date of the movie
+        public static final String COLUMN_RELEASE_DATE = "release_date";
+        // ID of the movie poster
+        public static final String COLUMN_POSTER = "poster";
+        // ID of the movie thumbnail poster
+        public static final String COLUMN_THUMB = "thumb";
+        // Rating of movie
+        public static final String COLUMN_RATING = "rating";
+    }
+}
---
+++ app/src/main/java/com/example/android/movies/data/MovieDBHelper.java
@@ -0,0 +1,49 @@
+package com.example.android.movies.data;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import com.example.android.movies.data.MovieContract.MovieEntry;
+/**
+ * Manages a local database for movie data.
+ */
+public class MovieDBHelper extends SQLiteOpenHelper {
+
+    // If you change the database schema, you must increment the database version.
+    private static final int DATABASE_VERSION = 2;
+
+    static final String DATABASE_NAME = "weather.db";
+
+    public MovieDBHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+
+        // Create a table to hold the movie data. Each movie item is defined
+        // by an ID, title, release date, user rating, synopsis, and poster
+        // and thumbnail paths
+        final String SQL_CREATE_MOVIE_TABLE = "CREATE TABLE " + MovieEntry.TABLE_NAME + " (" +
+                MovieEntry._ID + " INTEGER PRIMARY KEY," +
+                MovieEntry.COLUMN_TITLE + " TEXT NOT NULL, " +
+                MovieEntry.COLUMN_RELEASE_DATE + " TEXT NOT NULL, " +
+                MovieEntry.COLUMN_RATING + " REAL NOT NULL, " +
+                MovieEntry.COLUMN_SYNOPSIS + " TEXT NOT NULL, " +
+                MovieEntry.COLUMN_POSTER + " TEXT NOT NULL, " +
+                MovieEntry.COLUMN_THUMB + " TEXT NOT NULL " +
+                ");";
+
+        sqLiteDatabase.execSQL(SQL_CREATE_MOVIE_TABLE);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int oldVersion, int newVersion) {
+        // This database is only a cache for online data, so its upgrade policy is
+        // to simply to discard the data and start over whenever the version
+        // number of the DB changes
+        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " + MovieEntry.TABLE_NAME);
+        onCreate(sqLiteDatabase);
+    }
+}
